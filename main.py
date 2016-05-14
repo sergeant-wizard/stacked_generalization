6ac9b58b62c7f773c169b50d960596aa48cab132
Ryo Miyajima <sergeant.wizard@gmail.com>
2016-05-14 14:20:58 +0000

    each classes, with score 2.68902, 1605142313

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from stacked_generalization import StackedGeneralization
 from generalizer import Generalizer
 from sklearn import datasets # for debugging with iris
 
+n_classes = 39
+
 def load_bio_data():
     raw_train = numpy.loadtxt('train.csv', delimiter=',', skiprows=1)
     train_target = raw_train[:, 0]
@@ -22,10 +24,11 @@ def load_iris_data():
     test_data = iris.data # for simplicity
     return(train_data, train_target, test_data)
 
-def load_sfcc_data(year):
+def load_sfcc_data(class_index):
+    year = 0 # deprecated
     train_data = numpy.load("../sfcc/train_{}.npy".format(year))
     test_data = numpy.load("../sfcc/test_{}.npy".format(year))
-    train_target = train_data[:, 0].astype(int)
+    train_target = (train_data[:, 0] == class_index).astype(int)
     train_data = train_data[:, 1:]
     return(train_data, train_target, test_data)
 
@@ -39,9 +42,9 @@ def train_partial(sg, generalizers, save_predictions = True, suffix = ''):
     for generalizer_index, generalizer in enumerate(generalizers):
         if save_predictions:
             Generalizer.save_partial(generalizer.name() + suffix,
-                                              layer0_partial_guess[generalizer_index])
-        guess = layer0_partial_guess[generalizer_index, :, numpy.unique(sg.train_target)].T
-        print_logloss(generalizer.name(), sg.train_target, guess)
+                                              layer0_partial_guess[generalizer_index].astype(numpy.float16))
+        # guess = layer0_partial_guess[generalizer_index, :, numpy.unique(sg.train_target)].T
+        # print_logloss(generalizer.name(), sg.train_target, guess)
     return(layer0_partial_guess)
 
 def train_whole(sg, generalizers, save_predictions = True, suffix = ''):
@@ -49,7 +52,7 @@ def train_whole(sg, generalizers, save_predictions = True, suffix = ''):
     for generalizer_index, generalizer in enumerate(generalizers):
         if save_predictions:
             Generalizer.save_whole(generalizer.name() + suffix,
-                                   layer0_whole_guess[generalizer_index])
+                                   layer0_whole_guess[generalizer_index].astype(numpy.float16))
     return(layer0_whole_guess)
 
 def load_layer0(filenames):
@@ -59,15 +62,16 @@ def load_layer0(filenames):
                                         filename in filenames])
     return(layer0_partial_guess, layer0_whole_guess)
 
-def load_with_suffix(filenames, year):
-    return(load_layer0([filename + "_{}".format(year) for filename in filenames]))
+def load_with_suffix(filenames, class_index):
+    return(load_layer0([filename + "_{}".format(class_index) for filename in filenames]))
 
-def initialize_sg(year):
+def initialize_sg(class_index):
+    # local classes
+    n_classes = 2
     n_folds = 3
-    n_classes = 39
     # (train_data, train_target, test_data) = load_bio_data()
     # (train_data, train_target, test_data) = load_iris_data()
-    (train_data, train_target, test_data) = load_sfcc_data(year)
+    (train_data, train_target, test_data) = load_sfcc_data(class_index)
     return(StackedGeneralization(
         n_folds,
         train_data,
@@ -75,28 +79,21 @@ def initialize_sg(year):
         test_data,
         n_classes))
 
-def main():
-    num_year_partitions = 1
-    # # for ad-hoc training
-    # for year_partition_index in range(num_year_partitions):
-    #     sg = initialize_sg(year_partition_index)
-    #     generalizers = [RandomForest(), ExtraTrees()]
-    #     suffix = "_{}".format(year_partition_index)
-    #     layer0_partial_guess = train_partial(sg, generalizers, True, suffix)
-    #     del layer0_partial_guess
-    #     layer0_whole_guess = train_whole(sg, generalizers, True, suffix)
-    # return
+def layer0():
+    for class_index in range(n_classes):
+        sg = initialize_sg(class_index)
+        generalizers = [ExtraTrees()]
+        suffix = "_{}".format(class_index)
+        layer0_partial_guess = train_partial(sg, generalizers, True, suffix)
+        del layer0_partial_guess
+        layer0_whole_guess = train_whole(sg, generalizers, True, suffix)
 
-    header = 'Id,ARSON,ASSAULT,BAD CHECKS,BRIBERY,BURGLARY,DISORDERLY CONDUCT,DRIVING UNDER THE INFLUENCE,DRUG/NARCOTIC,DRUNKENNESS,EMBEZZLEMENT,EXTORTION,FAMILY OFFENSES,FORGERY/COUNTERFEITING,FRAUD,GAMBLING,KIDNAPPING,LARCENY/THEFT,LIQUOR LAWS,LOITERING,MISSING PERSON,NON-CRIMINAL,OTHER OFFENSES,PORNOGRAPHY/OBSCENE MAT,PROSTITUTION,RECOVERED VEHICLE,ROBBERY,RUNAWAY,SECONDARY CODES,SEX OFFENSES FORCIBLE,SEX OFFENSES NON FORCIBLE,STOLEN PROPERTY,SUICIDE,SUSPICIOUS OCC,TREA,TRESPASS,VANDALISM,VEHICLE THEFT,WARRANTS,WEAPON LAWS'
-    fmt = '%d,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f'
-
+def layer1():
     # loading predictions
-    n_classes = 39
-    result = numpy.empty((0, n_classes))
-    for year_partition_index in range(num_year_partitions):
-        print("processing year partition index{}".format(year_partition_index))
-        sg = initialize_sg(year_partition_index)
-        layer0_partial_guess, layer0_whole_guess = load_with_suffix(['random_forest', 'extra_trees'], year_partition_index)
+    for class_index in range(n_classes):
+        print("processing class index{}".format(class_index))
+        sg = initialize_sg(class_index)
+        layer0_partial_guess, layer0_whole_guess = load_with_suffix(['random_forest', 'extra_trees'], class_index)
 
         prediction = LogisticRegression().guess(
             numpy.hstack(layer0_partial_guess),
@@ -105,9 +102,20 @@ def main():
         expanded = Generalizer.expand_all_classes(prediction,
                                                   numpy.unique(sg.train_target),
                                                   sg.n_classes)
-        result = numpy.vstack((result, expanded))
+        numpy.save("layer1_result_{}".format(class_index), expanded)
 
-    id_column = numpy.array(range(len(result)))
+def concatenate():
+    header = 'Id,ARSON,ASSAULT,BAD CHECKS,BRIBERY,BURGLARY,DISORDERLY CONDUCT,DRIVING UNDER THE INFLUENCE,DRUG/NARCOTIC,DRUNKENNESS,EMBEZZLEMENT,EXTORTION,FAMILY OFFENSES,FORGERY/COUNTERFEITING,FRAUD,GAMBLING,KIDNAPPING,LARCENY/THEFT,LIQUOR LAWS,LOITERING,MISSING PERSON,NON-CRIMINAL,OTHER OFFENSES,PORNOGRAPHY/OBSCENE MAT,PROSTITUTION,RECOVERED VEHICLE,ROBBERY,RUNAWAY,SECONDARY CODES,SEX OFFENSES FORCIBLE,SEX OFFENSES NON FORCIBLE,STOLEN PROPERTY,SUICIDE,SUSPICIOUS OCC,TREA,TRESPASS,VANDALISM,VEHICLE THEFT,WARRANTS,WEAPON LAWS'
+    fmt = '%d,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f,%1.6f'
+
+    n_rows = 884262
+    result = numpy.empty((n_rows, n_classes), dtype=numpy.float16)
+    for class_index in range(n_classes):
+        print("class_index: {}".format(class_index))
+        class_result = numpy.load("layer1_result_{}.npy".format(class_index))
+        result[:, class_index] = class_result[:, 1]
+
+    id_column = numpy.array(range(result.shape[0]))
 
     numpy.savetxt(
         'predicted.csv',
@@ -117,4 +125,4 @@ def main():
         comments='')
 
 if __name__ == "__main__":
-    main()
+    concatenate()
