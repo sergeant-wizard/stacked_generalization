e58e1bb885f8e91ab806577cde4a7ad81965b64c
Ryo Miyajima <sergeant.wizard@gmail.com>
2016-05-18 08:48:05 +0000

    use multi class for xgboost

--- gen_xgboost.py
+++ gen_xgboost.py
@@ -9,8 +9,9 @@ class Xgboost(Generalizer):
     def train(self, data, label):
         dtrain = xgb.DMatrix(data, label = label)
         params = {
-            'objective': 'binary:logistic',
-            'eta': 1.0
+            'objective': 'multi:softprob',
+            'eta': 1.0,
+            'num_class': 39
         }
         num_round = 10
         self.model = xgb.train(params, dtrain, num_round)
@@ -18,4 +19,4 @@ class Xgboost(Generalizer):
     def predict(self, data):
         dmatrix = xgb.DMatrix(data)
         predicted = self.model.predict(dmatrix)
-        return(numpy.vstack((1 - predicted, predicted)).T)
+        return(predicted)
